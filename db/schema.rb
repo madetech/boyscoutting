33e1bbe995db272066b150b0a5ab36ea137247f1
Emile <emile.swarts123@gmail.com>
2016-09-29 12:29:37 +0100

    db stuff

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,18 @@
 
 ActiveRecord::Schema.define(version: 20160922092538) do
 
-  create_table "merits", force: :cascade do |t|
+  create_table "merits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username"
     t.string   "name"
     t.string   "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
