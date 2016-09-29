cb21d5a2ecfdd88c0cbea16abda614beab42ee30
Emile <emile.swarts123@gmail.com>
2016-09-29 14:05:27 +0100

    Use comments service

--- app/controllers/pr_events_controller.rb
+++ app/controllers/pr_events_controller.rb
@@ -5,14 +5,7 @@ class PrEventsController < ApplicationController
     pr_service = GitHub::PullRequestAction.new(params)
 
     if pr_service.is_pr? && pr_service.state == 'closed'
-      url = URI.parse(pr_service.comments_url)
-      req = Net::HTTP::Get.new(url.to_s)
-      @response = Net::HTTP.start(url.host, url.port) do |http|
-        http.request(req)
-      end
+      comments = GitHub::PullRequestComments.fetch(pr_service.comments_url)
     end
-  end
-
-  def index
   end
 end
