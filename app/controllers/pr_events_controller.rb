2900ec20b71118ace641efb446f29dea6d00bc44
Emile <emile.swarts123@gmail.com>
2016-09-29 13:51:11 +0100

    Test post create

--- app/controllers/pr_events_controller.rb
+++ app/controllers/pr_events_controller.rb
@@ -1,5 +1,16 @@
+require 'net/http'
+
 class PrEventsController < ApplicationController
   def create
+    pr_service = GitHub::PullRequestAction.new(params)
+
+    if pr_service.is_pr? && pr_service.state == 'closed'
+      url = URI.parse(pr_service.comments_url)
+      req = Net::HTTP::Get.new(url.to_s)
+      @response = Net::HTTP.start(url.host, url.port) do |http|
+        http.request(req)
+      end
+    end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  post '/pr-event', to: 'pr_events#create'
+  match '/pr-event', to: 'pr_events#create'
   get '/pr-event', to: 'pr_events#index'
 end
